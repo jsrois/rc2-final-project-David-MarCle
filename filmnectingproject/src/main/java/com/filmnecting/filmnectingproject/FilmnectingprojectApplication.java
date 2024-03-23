028b106177fbcea58efa667a4577ecd9356dc599
David-MarCle <martinezclementedavid@gmail.com>
2024-03-23 12:23:51 +0100

    back init

---
+++ filmnectingproject/src/main/java/com/filmnecting/filmnectingproject/FilmnectingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.filmnecting.filmnectingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmnectingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmnectingprojectApplication.class, args);
+	}
+
+}
---
+++ filmnectingproject/src/test/java/com/filmnecting/filmnectingproject/FilmnectingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmnecting.filmnectingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmnectingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
